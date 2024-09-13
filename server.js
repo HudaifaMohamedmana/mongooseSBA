a082b625afc3d3b8c464a472f9a73d9d39c2eee3
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-09-13 15:42:16 -0400

    the express frame

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+//----------------------------imports 
+
+
+//----------------------------server 
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
